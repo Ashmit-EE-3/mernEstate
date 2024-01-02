91a6e195c891cf1a65879f66157844768b331c2c
Ashmit-EE-3 <12214050@nitkkr.ac.in>
2024-01-02 14:28:11 +0530

    connected to database

--- api/index.js
+++ api/index.js
@@ -1,11 +1,19 @@
 const express = require('express')
-
+require('dotenv').config()
+const mongoose = require('mongoose')
 const app = express()
 
-app.get('/hello',(req,res)=>{
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+    console.log("Connected to MONGO DB...")
+})
+.catch((err) => {
+    console.log(err)
+})
+
+app.get('/hello', (req, res) => {
     res.send("Hello World")
 })
 
-app.listen(3000,()=>{
-    console.log("Server is listening on port 3000")
-})
+
+app.listen(3000, () => { console.log("Server is listening on port 3000....") })   
