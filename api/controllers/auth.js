f4accb1879eb82610ecd285c53ad01b93aa9b4ba
Ashmit-EE-3 <12214050@nitkkr.ac.in>
2024-01-02 16:25:41 +0530

    create error handling middleware and functions

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,8 +1,9 @@
 const User = require("../models/user")
-const bcryptjs = require('bcryptjs')
+const bcryptjs = require('bcryptjs');
+const errorHandler = require("../utils/error");
 
 
-const signup = async (req, res) => {
+const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
     const hashedPassword = bcryptjs.hashSync(password, 10);
 
@@ -10,8 +11,7 @@ const signup = async (req, res) => {
         const user = await User.create({ username, email, password: hashedPassword })
         res.status(201).json({ success: true, data: user })
     } catch (error) {
-        console.log(error)
-        res.status(500).json(error.message)
+        next(error)
     }
 }
 
--- api/index.js
+++ api/index.js
@@ -16,4 +16,12 @@ mongoose.connect(process.env.MONGO_URI)
 app.use(express.json())
 app.use('/api/v1/user',userRouter)
 app.use('/api/v1/auth',authRouter)
+
+app.use((err,req,res,next)=>{
+    const statusCode = err.statusCode || 500 ; 
+    const message = err.message || "Internal Server Error" ; 
+    return res.status(statusCode).json({success:false, statusCode, message})
+})
+
+
 app.listen(3000, () => { console.log("Server is listening on port 3000....") })   
