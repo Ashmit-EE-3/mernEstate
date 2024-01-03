d47f4ebcf18d834be1deec610f0a7a63d0df569b
Ashmit-EE-3 <12214050@nitkkr.ac.in>
2024-01-03 21:56:10 +0530

    add redux persist functionality

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
-import { store } from './redux/store.js'
+import { persistor, store } from './redux/store.js'
+import { PersistGate } from 'redux-persist/integration/react'
 
 class ErrorBoundary extends React.Component {
   constructor(props) {
@@ -40,7 +41,9 @@ class ErrorBoundary extends React.Component {
 ReactDOM.createRoot(document.getElementById('root')).render(
     <ErrorBoundary>    
       <Provider store={store}>
+      <PersistGate loading = {null} persistor = {persistor}>
       <App />
+      </PersistGate>
       </Provider>
     </ErrorBoundary>
   
