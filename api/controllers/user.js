dc546e127517d762a8b2354349cf7dba1adca32b
Ashmit-EE-3 <12214050@nitkkr.ac.in>
2024-01-05 15:00:00 +0530

    create delete user api route

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,33 +1,46 @@
 const User = require("../models/user");
 const errorHandler = require("../utils/error")
-const bcryptjs = require('bcryptjs') ; 
+const bcryptjs = require('bcryptjs');
 
-const test = (req,res)=>{
-    res.json({message : "API route is working"})
-} 
+const test = (req, res) => {
+    res.json({ message: "API route is working" })
+}
 
-const updateUser = async (req,res,next)=>{
-    if (req.user.id !== req.params.id) return next(errorHandler(401,"You can only update your own account"))
+const updateUser = async (req, res, next) => {
+    if (req.user.id !== req.params.id) return next(errorHandler(401, "You can only update your own account"))
 
     try {
-        if(req.body.password){
-            req.body.password = bcryptjs.hashSync(req.body.password,10) 
+        if (req.body.password) {
+            req.body.password = bcryptjs.hashSync(req.body.password, 10)
         }
 
         const updatedUser = await User.findByIdAndUpdate(req.params.id, {
-            $set : {
-                username : req.body.username, 
-                email :  req.body.email, 
-                password : req.body.password, 
-                avatar : req.body.avatar, 
+            $set: {
+                username: req.body.username,
+                email: req.body.email,
+                password: req.body.password,
+                avatar: req.body.avatar,
             }
-        }, {new : true})
+        }, { new: true })
 
-        const {password, ...others} = updatedUser._doc 
-        res.status(200).json(others) ;
+        const { password, ...others } = updatedUser._doc
+        res.status(200).json(others);
     } catch (error) {
         next(error)
     }
 }
 
-module.exports = {test ,updateUser}
+const deleteUser = async (req, res, next) => {
+
+    try {
+        if (req.user.id !== req.params.id) return next(errorHandler(401, "You can delete your own account only !"))
+
+        await User.findByIdAndDelete(req.params.id);
+        res.status(200).json({ success: true, msg: "User has been deleted successfully !" })
+    }
+    catch (error) {
+        next(error);
+    }
+}
+
+module.exports = { test, updateUser, deleteUser }
--- api/routes/user.js
+++ api/routes/user.js
@@ -1,9 +1,10 @@
 const express = require('express')
-const {test, updateUser} = require('../controllers/user');
+const {test, updateUser, deleteUser} = require('../controllers/user');
 const verifyUser = require('../utils/verifyUser');
 const router = express.Router()
 
 router.route('/test').get(test)
 router.route('/update/:id').post(verifyUser, updateUser) ;
+router.route('/delete/:id').delete(verifyUser, deleteUser) ; 
 
 module.exports = router 
