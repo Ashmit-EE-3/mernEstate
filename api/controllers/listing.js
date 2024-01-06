6d9b35ebdac66993e5ad14b79dbf19df2c08f75d
Ashmit-EE-3 <12214050@nitkkr.ac.in>
2024-01-06 17:34:10 +0530

    create update listing api route

--- api/controllers/listing.js
+++ api/controllers/listing.js
@@ -26,4 +26,20 @@ const deleteListing = async (req,res,next)=>{
     }
 }
 
-module.exports = {createListing, deleteListing}
+const updateListing = async (req,res,next)=>{
+    const listing = await Listing.findById(req.params.id) ;
+
+    if (!listing) return next(errorHandler(404,"Listing doesn't exist"))
+
+    if (req.user.id !== listing.userRef) return next(errorHandler(401, "You can edit your listings only!"))
+
+    try {
+        const update = await Listing.findByIdAndUpdate(req.params.id, req.body) ; 
+        res.status(200).json(update) ; 
+
+    } catch (error) {
+        next(error) ; 
+    }
+}
+
+module.exports = {createListing, deleteListing, updateListing}
--- api/routes/listing.js
+++ api/routes/listing.js
@@ -1,10 +1,11 @@
 const express = require('express') ;
-const { createListing, deleteListing } = require('../controllers/listing');
+const { createListing, deleteListing, updateListing } = require('../controllers/listing');
 const verifyUser = require('../utils/verifyUser');
 
 const router = express.Router() ;
 
 router.route('/create').post(verifyUser, createListing) ; 
 router.route('/delete/:id').delete(verifyUser, deleteListing) ; 
+router.route('/update/:id').post(verifyUser, updateListing) ; 
 
 module.exports = router ; 
